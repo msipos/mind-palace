b76217168c0eed2f2641e8d47c628d32c7c1b4c0
Maksim Sipos <msipos@mailc.net>
2020-09-30 22:06:53 +0100

    add NoteActions helper

--- frontend/index.js
+++ frontend/index.js
@@ -9,7 +9,9 @@ import RepeatPage from "./src/pages/RepeatPage";
 import './src/site.js';
 import 'bulma/css/bulma.css';
 import './src/site.css';
+import NoteActions from "./src/components/NoteActions";
 
+// Various React components and pages
 window.mpReact = {
     components: {
         NoteButton: NoteButton,
@@ -26,6 +28,9 @@ window.mpReact = {
             React.createElement(component, props),
             document.getElementById(htmlId),
         )
-    }
+    },
 };
+
+// Global variable that is used to send information to the backend, populated in the specific HTML pages.
+window.actions = new NoteActions();
 
---
+++ frontend/src/components/NoteActions.js
@@ -0,0 +1,47 @@
+import {fetchPost} from "../utils";
+
+class NoteActions {
+    constructor() {
+        // URL where to submit note actions
+        this.urlNoteAction = null;
+
+        // URL for note editing
+        this.urlNoteEdit = null;
+
+        // URL where to list the note's parent collection
+        this.urlListCollection = null;
+    }
+
+    async submitNoteAction(action) {
+        if (action !== "delete" && action !== "archive" && action !== "unarchive") {
+            console.error("Invalid action: ", action);
+            throw {description: "Invalid action", action: action};
+        }
+
+        if (!this.urlNoteAction) {
+            console.error("Must set urlNoteAction");
+            throw {description: "Did not set urlNoteAction"};
+        }
+
+        return await fetchPost(this.urlNoteAction, {action: action});
+    }
+
+    redirectToListCollection() {
+        if (!this.urlListCollection) {
+            console.error("Must set urlListCollection");
+            throw {description: "Did not set urlListCollection"};
+        }
+        window.location = this.urlListCollection;
+    }
+
+    redirectToNoteEdit() {
+        if (!this.urlNoteEdit) {
+            console.error("Must set urlNoteEdit");
+            throw {description: "Did not set urlNoteEdit"};
+        }
+        window.location = this.urlNoteEdit;
+
+    }
+}
+
+export default NoteActions;
