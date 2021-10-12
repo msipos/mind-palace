603e52a650966c104d8806664bc7738021ff0133
Maksim Sipos <msipos@mailc.net>
2021-10-12 08:01:06 +0100

    More to make docker run simply

---
+++ mind_palace/mind_palace_main/management/commands/create_admin.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Create an admin user'
+
+    def handle(self, *args, **options):
+        print('Creating admin user...')
+        try:
+            User.objects.create_superuser('admin', password='change_my_password')
+            print('... created.')
+        except:
+            print('... admin user already exists.')
